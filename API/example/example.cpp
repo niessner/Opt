c933d059a45135a0e205227c1e5fae253f5378c4
mdfisher <mdfisher@bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23>
2015-03-24 23:03:14 +0000

    git-svn-id: https://scm9.informatik.uni-erlangen.de/simanies/svn/Optimization@49 bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23

--- API/example/example.cpp
+++ API/example/example.cpp
@@ -2,20 +2,28 @@ extern "C" {
 #include "Opt.h"
 }
 
+#include <iostream>
 
-#include <stdio.h>
+using namespace std;
 
 int main() {
-    printf("this is main\n");
     OptState * S =  Opt_NewState();
-    
     uint64_t dims[] = {1024,512};
     
     ImageBinding * A = Opt_ImageBind(S,NULL,1024,sizeof(double));
     ImageBinding * x = Opt_ImageBind(S,NULL,1,sizeof(double));
     ImageBinding * b = Opt_ImageBind(S,NULL,1,sizeof(double));
     
+    cout << "start Opt_ProblemDefine" << endl;
     Problem * prob = Opt_ProblemDefine(S,"example.t","gradientdescent",NULL);
+
+    if (!prob)
+    {
+        cout << "Opt_ProblemDefine failed" << endl;
+        cin.get();
+        return 1;
+    }
+    
     Plan * plan = Opt_ProblemPlan(S,prob,dims);
     
     ImageBinding * images[] = {x,A,b};
--- API/example/example.t
+++ API/example/example.t
@@ -11,12 +11,14 @@ B = opt.Image(double,R,1)
 
 terra cost(i : uint64, j : uint64, x : X, a : A, b : B)
     IO.printf("the cost function, %d x %d\n",int(i),int(j))
-    IO.printf("A(%d,%d) X(%d,%d) B(%d,%d)\n",int(a.R),int(a.C),int(x.R),int(x.C),int(b.R),int(b.C))
+    --IO.printf("A(%d,%d) X(%d,%d) B(%d,%d)\n",int(a.R),int(a.C),int(x.R),int(x.C),int(b.R),int(b.C))
+    return 1.0
 end
 
 terra gradient(i : uint64, j : uint64, x : X, a : A, b : B)
     IO.printf("the grad function, %d x %d\n",int(i),int(j))
-    IO.printf("A(%d,%d) X(%d,%d) B(%d,%d)\n",int(a.R),int(a.C),int(x.R),int(x.C),int(b.R),int(b.C))
+    --IO.printf("A(%d,%d) X(%d,%d) B(%d,%d)\n",int(a.R),int(a.C),int(x.R),int(x.C),int(b.R),int(b.C))
+    return 1.0
 end
 
 return { dims = {R,C},
