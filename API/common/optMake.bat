eb8deb6460051e6b5bc7f1c97366c20a4236d1fc
Mx7f <Mx7f@bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23>
2015-05-18 21:30:14 +0000

    Abstracting optMake.bat
    git-svn-id: https://scm9.informatik.uni-erlangen.de/simanies/svn/Optimization@297 bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23

---
+++ API/common/optMake.bat
@@ -0,0 +1,14 @@
+echo Remaking Opt.dll
+set TERRAHOME=%1
+set TERRA=%TERRAHOME%\release\terra
+set LUAHOME=%TERRAHOME%\..\LuaJIT-2.0.3
+set OPT_DIR=%2
+set TARGET_DIR=%3
+
+
+cd %OPT_DIR%
+%TERRA% ..\src\createWrapper.t Opt ..\src\windowsErrorContextWrapper.t
+cd %TARGET_DIR%
+copy %OPT_DIR%\Opt.dll .
+copy %OPT_DIR%\Opt.lib .
+copy %OPT_DIR%\Opt.h .
