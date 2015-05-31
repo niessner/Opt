d3a9de7fc94e4e2d3ecec2a2daf13b4a420f65b9
simanies <simanies@bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23>
2015-05-31 04:18:33 +0000

    git-svn-id: https://scm9.informatik.uni-erlangen.de/simanies/svn/Optimization@459 bfabd5f7-08ac-4c2d-9fe7-a7df3bb4fc23

---
+++ Examples/ImageWarping/src/TerraSolverPoissonImageEditing.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cassert>
+
+#include "cutil.h"
+
+extern "C" {
+#include "../Opt.h"
+}
+
+class TerraSolverWarpingFloat4 {
+
+public:
+	TerraSolverWarpingFloat4(unsigned int width, unsigned int height, const std::string& terraFile, const std::string& optName) : m_optimizerState(nullptr), m_problem(nullptr), m_plan(nullptr)
+	{
+		m_optimizerState = Opt_NewState();
+		m_problem = Opt_ProblemDefine(m_optimizerState, terraFile.c_str(), optName.c_str(), NULL);
+
+
+		uint64_t strides[] = { width * sizeof(float4), width * sizeof(float4) };
+		uint64_t elemsizes[] = { sizeof(float4), sizeof(float4) };
+		uint64_t dims[] = { width, height };
+		m_plan = Opt_ProblemPlan(m_optimizerState, m_problem, dims, elemsizes, strides, nullptr, nullptr, nullptr);
+
+		assert(m_optimizerState);
+		assert(m_problem);
+		assert(m_plan);
+	}
+
+	~TerraSolverWarpingFloat4()
+	{
+		if (m_plan) {
+			Opt_PlanFree(m_optimizerState, m_plan);
+		}
+
+		if (m_problem) {
+			Opt_ProblemDelete(m_optimizerState, m_problem);
+		}
+
+	}
+
+	void solve(float4* d_unknown, float4* d_target, unsigned int nNonLinearIterations, unsigned int nLinearIterations, unsigned int nBlockIterations, float weightFit, float weightReg)
+	{
+
+		void* data[] = {d_unknown, d_target};
+		void* solverParams[] = { &nNonLinearIterations, &nLinearIterations, &nBlockIterations };
+
+		float weightFitSqrt = sqrt(weightFit);
+		float weightRegSqrt = sqrt(weightReg);
+		void* problemParams[] = { &weightFitSqrt, &weightRegSqrt };
+
+
+		//Opt_ProblemInit(m_optimizerState, m_plan, data, NULL, problemParams, (void**)&solverParams);
+		//while (Opt_ProblemStep(m_optimizerState, m_plan, data, NULL, problemParams, NULL));
+		Opt_ProblemSolve(m_optimizerState, m_plan, data, NULL, problemParams, solverParams);
+	}
+
+private:
+	OptState*	m_optimizerState;
+	Problem*	m_problem;
+	Plan*		m_plan;
+};
