7f717c084287076c38d570b15c52bb95098a32fb
Michael <mikemx7f@gmail.com>
2016-01-08 14:17:09 -0800

    3D Marker Placement.

---
+++ Tools/3DARAPMarkerPlacement/source/App.cpp
@@ -0,0 +1,222 @@
+/** \file App.cpp */
+#include "App.h"
+
+// Tells C++ to invoke command-line main() function even on OS X and Win32.
+G3D_START_AT_MAIN();
+
+int main(int argc, const char* argv[]) {
+    {
+        G3DSpecification g3dSpec;
+        g3dSpec.audio = false;
+        initGLG3D(g3dSpec);
+    }
+
+    GApp::Settings settings(argc, argv);
+
+    // Change the window and other startup parameters by modifying the
+    // settings class.  For example:
+    settings.window.caption             = argv[0];
+    // settings.window.debugContext     = true;
+
+    // settings.window.width              =  854; settings.window.height       = 480;
+    // settings.window.width            = 1024; settings.window.height       = 768;
+     settings.window.width            = 1280; settings.window.height       = 720;
+//    settings.window.width               = 1920; settings.window.height       = 1080;
+    // settings.window.width            = OSWindow::primaryDisplayWindowSize().x; settings.window.height = OSWindow::primaryDisplayWindowSize().y;
+    settings.window.fullScreen          = false;
+    settings.window.resizable           = ! settings.window.fullScreen;
+    settings.window.framed              = ! settings.window.fullScreen;
+
+    // Set to true for a significant performance boost if your app can't render at 60fps,
+    // or if you *want* to render faster than the display.
+    settings.window.asynchronous        = false;
+
+    settings.depthGuardBandThickness    = Vector2int16(64, 64);
+    settings.colorGuardBandThickness    = Vector2int16(0, 0);
+    settings.dataDir                    = FileSystem::currentDirectory();
+    settings.screenshotDirectory        = "../journal/";
+
+    settings.renderer.deferredShading = false;
+    settings.renderer.orderIndependentTransparency = false;
+
+
+    return App(settings).run();
+}
+
+
+App::App(const GApp::Settings& settings) : GApp(settings) {
+}
+
+
+// Called before the application loop begins.  Load data here and
+// not in the constructor so that common exceptions will be
+// automatically caught.
+void App::onInit() {
+    GApp::onInit();
+    setFrameDuration(1.0f / 120.0f);
+
+    // Call setScene(shared_ptr<Scene>()) or setScene(MyScene::create()) to replace
+    // the default scene here.
+    
+    showRenderingStats      = true;
+
+    makeGUI();
+    // For higher-quality screenshots:
+    // developerWindow->videoRecordDialog->setScreenShotFormat("PNG");
+    // developerWindow->videoRecordDialog->setCaptureGui(false);
+    developerWindow->cameraControlWindow->moveTo(Point2(developerWindow->cameraControlWindow->rect().x0(), 0));
+    loadScene(
+//        "Hair"
+         //"G3D Sponza"
+        "G3D Cornell Box" // Load something simple
+        //developerWindow->sceneEditorWindow->selectedSceneName()  // Load the first scene encountered 
+        );
+}
+
+
+void App::makeGUI() {
+    // Initialize the developer HUD (using the existing scene)
+    createDeveloperHUD();
+    debugWindow->setVisible(true);
+    developerWindow->videoRecordDialog->setEnabled(true);
+
+    GuiPane* infoPane = debugPane->addPane("Info", GuiTheme::ORNATE_PANE_STYLE);
+    
+    // Example of how to add debugging controls
+    infoPane->addLabel("You can add GUI controls");
+    infoPane->addLabel("in App::onInit().");
+    infoPane->addButton("Exit", this, &App::endProgram);
+    infoPane->pack();
+
+    // More examples of debugging GUI controls:
+    // debugPane->addCheckBox("Use explicit checking", &explicitCheck);
+    // debugPane->addTextBox("Name", &myName);
+    // debugPane->addNumberBox("height", &height, "m", GuiTheme::LINEAR_SLIDER, 1.0f, 2.5f);
+    // button = debugPane->addButton("Run Simulator");
+
+    debugWindow->pack();
+    debugWindow->setRect(Rect2D::xywh(0, 0, (float)window()->width(), debugWindow->rect().height()));
+}
+
+
+void App::onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& allSurfaces) {
+    if (!scene()) {
+        if ((submitToDisplayMode() == SubmitToDisplayMode::MAXIMIZE_THROUGHPUT) && (!rd->swapBuffersAutomatically())) {
+            swapBuffers();
+        }
+        rd->clear();
+        rd->pushState(); {
+            rd->setProjectionAndCameraMatrix(activeCamera()->projection(), activeCamera()->frame());
+            drawDebugShapes();
+        } rd->popState();
+        return;
+    }
+
+    GBuffer::Specification gbufferSpec = m_gbufferSpecification;
+    extendGBufferSpecification(gbufferSpec);
+    m_gbuffer->setSpecification(gbufferSpec);
+    m_gbuffer->resize(m_framebuffer->width(), m_framebuffer->height());
+    m_gbuffer->prepare(rd, activeCamera(), 0, -(float)previousSimTimeStep(), m_settings.depthGuardBandThickness, m_settings.colorGuardBandThickness);
+
+    m_renderer->render(rd, m_framebuffer, scene()->lightingEnvironment().ambientOcclusionSettings.enabled ? m_depthPeelFramebuffer : shared_ptr<Framebuffer>(),
+        scene()->lightingEnvironment(), m_gbuffer, allSurfaces);
+
+    // Debug visualizations and post-process effects
+    rd->pushState(m_framebuffer); {
+        // Call to make the App show the output of debugDraw(...)
+        rd->setProjectionAndCameraMatrix(activeCamera()->projection(), activeCamera()->frame());
+        drawDebugShapes();
+        const shared_ptr<Entity>& selectedEntity = (notNull(developerWindow) && notNull(developerWindow->sceneEditorWindow)) ? developerWindow->sceneEditorWindow->selectedEntity() : shared_ptr<Entity>();
+        scene()->visualize(rd, selectedEntity, allSurfaces, sceneVisualizationSettings(), activeCamera());
+
+        // Post-process special effects
+        m_depthOfField->apply(rd, m_framebuffer->texture(0), m_framebuffer->texture(Framebuffer::DEPTH), activeCamera(), m_settings.depthGuardBandThickness - m_settings.colorGuardBandThickness);
+
+        m_motionBlur->apply(rd, m_framebuffer->texture(0), m_gbuffer->texture(GBuffer::Field::SS_EXPRESSIVE_MOTION),
+            m_framebuffer->texture(Framebuffer::DEPTH), activeCamera(),
+            m_settings.depthGuardBandThickness - m_settings.colorGuardBandThickness);
+    } rd->popState();
+
+    // We're about to render to the actual back buffer, so swap the buffers now.
+    // This call also allows the screenshot and video recording to capture the
+    // previous frame just before it is displayed.
+    if (submitToDisplayMode() == SubmitToDisplayMode::MAXIMIZE_THROUGHPUT) {
+        swapBuffers();
+    }
+
+    // Clear the entire screen (needed even though we'll render over it, since
+    // AFR uses clear() to detect that the buffer is not re-used.)
+    rd->clear();
+
+    // Perform gamma correction, bloom, and SSAA, and write to the native window frame buffer
+    m_film->exposeAndRender(rd, activeCamera()->filmSettings(), m_framebuffer->texture(0));
+}
+
+
+void App::onAI() {
+    GApp::onAI();
+    // Add non-simulation game logic and AI code here
+}
+
+
+void App::onNetwork() {
+    GApp::onNetwork();
+    // Poll net messages here
+}
+
+
+void App::onSimulation(RealTime rdt, SimTime sdt, SimTime idt) {
+    GApp::onSimulation(rdt, sdt, idt);
+
+    // Example GUI dynamic layout code.  Resize the debugWindow to fill
+    // the screen horizontally.
+    debugWindow->setRect(Rect2D::xywh(0, 0, (float)window()->width(), debugWindow->rect().height()));
+}
+
+
+bool App::onEvent(const GEvent& event) {
+    // Handle super-class events
+    if (GApp::onEvent(event)) { return true; }
+
+    // If you need to track individual UI events, manage them here.
+    // Return true if you want to prevent other parts of the system
+    // from observing this specific event.
+    //
+    // For example,
+    // if ((event.type == GEventType::GUI_ACTION) && (event.gui.control == m_button)) { ... return true; }
+    // if ((event.type == GEventType::KEY_DOWN) && (event.key.keysym.sym == GKey::TAB)) { ... return true; }
+
+    return false;
+}
+
+
+void App::onUserInput(UserInput* ui) {
+    GApp::onUserInput(ui);
+    (void)ui;
+    // Add key handling here based on the keys currently held or
+    // ones that changed in the last frame.
+}
+
+
+void App::onPose(Array<shared_ptr<Surface> >& surface, Array<shared_ptr<Surface2D> >& surface2D) {
+    GApp::onPose(surface, surface2D);
+
+    // Append any models to the arrays that you want to later be rendered by onGraphics()
+}
+
+
+void App::onGraphics2D(RenderDevice* rd, Array<shared_ptr<Surface2D> >& posed2D) {
+    // Render 2D objects like Widgets.  These do not receive tone mapping or gamma correction.
+    Surface2D::sortAndRender(rd, posed2D);
+}
+
+
+void App::onCleanup() {
+    // Called after the application loop ends.  Place a majority of cleanup code
+    // here instead of in the constructor so that exceptions can be caught.
+}
+
+
+void App::endProgram() {
+    m_endProgram = true;
+}
