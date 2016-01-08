7c395505f6745a2aee05112aceb07b5fbcd61b7c
Michael <mikemx7f@gmail.com>
2016-01-08 13:49:53 -0800

    Visualizer

---
+++ Tools/2DARAPVisualizer/source/App.h
@@ -0,0 +1,49 @@
+/**
+  \file App.h
+
+  The G3D 10.00 default starter app is configured for OpenGL 3.3 and
+  relatively recent GPUs.
+ */
+#ifndef App_h
+#define App_h
+#include <G3D/G3DAll.h>
+
+/** Application framework. */
+class App : public GApp {
+protected:
+    String              m_directory;
+
+    shared_ptr<Texture> m_input;
+    shared_ptr<Texture> m_output;
+    shared_ptr<Framebuffer> m_visualizationFB;
+
+    Array<Point4int32> m_handles;
+
+    /** Called from onInit */
+    void makeGUI();
+
+public:
+    
+    App(const GApp::Settings& settings = GApp::Settings());
+
+    virtual void onInit() override;
+    virtual void onAI() override;
+    virtual void onNetwork() override;
+    virtual void onSimulation(RealTime rdt, SimTime sdt, SimTime idt) override;
+    virtual void onPose(Array<shared_ptr<Surface> >& posed3D, Array<shared_ptr<Surface2D> >& posed2D) override;
+
+    // You can override onGraphics if you want more control over the rendering loop.
+    // virtual void onGraphics(RenderDevice* rd, Array<shared_ptr<Surface> >& surface, Array<shared_ptr<Surface2D> >& surface2D) override;
+
+    virtual void onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& surface3D) override;
+    virtual void onGraphics2D(RenderDevice* rd, Array<shared_ptr<Surface2D> >& surface2D) override;
+
+    virtual bool onEvent(const GEvent& e) override;
+    virtual void onUserInput(UserInput* ui) override;
+    virtual void onCleanup() override;
+    
+    /** Sets m_endProgram to true. */
+    virtual void endProgram();
+};
+
+#endif
