e8d548d76f8d3119825297d214b722076e8c3fc5
Zachary DeVito <zdevito@stanford.edu>
2016-04-27 19:39:54 -0700

    more mLib fixes

--- Examples/ImageWarpingSimple/src/mLibSource.cpp
+++ Examples/ImageWarpingSimple/src/mLibSource.cpp
@@ -4,6 +4,6 @@
 #include "mLib/common.cpp"
 #include "mLib/utility.cpp"
 #define LODEPNG_COMPILE_ENCODER
-#include "mlib/lodepng.cpp"
+#include "mLib/lodepng.cpp"
 #include "mLib/imageLoaderLodePNG.cpp"
 
