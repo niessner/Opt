5dd5fc98e9dddff4e8ff45486f7675f693e1581c
Michael <mikemx7f@gmail.com>
2016-10-12 12:43:50 -0700

    Setting up for CERES comparisons

--- Examples/SimpleCERESComparison/src/CeresSolver.cpp
+++ Examples/SimpleCERESComparison/src/CeresSolver.cpp
@@ -73,21 +73,26 @@ void CeresSolver::solve(
     //faster methods
     options.num_threads = 1;
     options.num_linear_solver_threads = 1;
-    //options.linear_solver_type = ceres::LinearSolverType::SPARSE_NORMAL_CHOLESKY; //7.2s
+    options.linear_solver_type = ceres::LinearSolverType::SPARSE_NORMAL_CHOLESKY; //7.2s
     //options.linear_solver_type = ceres::LinearSolverType::SPARSE_SCHUR; //10.0s
 
     //slower methods
     //options.linear_solver_type = ceres::LinearSolverType::ITERATIVE_SCHUR; //40.6s
-    options.linear_solver_type = ceres::LinearSolverType::CGNR; //46.9s
+    //options.linear_solver_type = ceres::LinearSolverType::CGNR; //46.9s
 
     //options.min_linear_solver_iterations = linearIterationMin;
     options.max_num_iterations = 10000;
     options.function_tolerance = 0.00001;
     options.gradient_tolerance = 1e-4 * options.function_tolerance;
 
-    //options.min_lm_diagonal = 1.0f;
-    //options.min_lm_diagonal = options.max_lm_diagonal;
-    //options.max_lm_diagonal = 10000000.0;
+    // Default values, reproduced here for clarity
+    options.trust_region_strategy_type = ceres::TrustRegionStrategyType::LEVENBERG_MARQUARDT;
+    options.initial_trust_region_radius = 1e4;
+    options.max_trust_region_radius = 1e16;
+    options.min_trust_region_radius = 1e-32;
+    options.min_relative_decrease = 1e-3;
+    options.min_lm_diagonal = 1e-6;
+    options.max_lm_diagonal = 1e32;
 
     //problem.Evaluate(Problem::EvaluateOptions(), &cost, nullptr, nullptr, nullptr);
     //cout << "Cost*2 start: " << cost << endl;
